d56af1ad680d71c9b9edfaae0607ba35ef107a6b
Ann Hrynyshyn <91032181+satesilka@users.noreply.github.com>
2022-06-16 19:17:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,4 @@ with open("access.log.txt", 'r') as file:
         if match := re.search(r"(72\.30\.79\.46|90\.7\.158\.134).+GET.+images.+200.+", line):
             print(line)
             count += 1
-print("arestovych has small dick just in case if you forget")
 print(f"count:{count}")
